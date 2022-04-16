8b1c02b6451d960d6149a60db0f6db923b8d2fcd
Ravikiran Kadam <ravi@techverito.com>
2022-04-16 18:44:48 +0530

    Added eslintrc

--- src/services/handlers/dependencyHandlers.ts
+++ src/services/handlers/dependencyHandlers.ts
@@ -1,9 +1,11 @@
 import { PackageJson } from '../../models/PackageJson';
+import fs from 'fs';
 
 export const dependencyHandlers = {
   eslint: (dependency: { name: string }, packageJson: PackageJson) => {
     if (dependency.name === 'eslint') {
       packageJson['scripts']['lint'] = 'eslint . --ext .js,.ts';
+      fs.copyFileSync('resources/app-base/.eslintrc.json', 'resources/app/.eslintrc.json');
     }
   },
   jest: (dependency: { name: string }, packageJson: PackageJson) => {
